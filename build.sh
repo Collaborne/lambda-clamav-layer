9bfa67093205b083fa42ac4bcc25a93d72ea677e
Andreas Kohn <andreas.kohn@gmail.com>
2021-06-11 16:21:48 +0200

    Include clamd and clamdscan in the layer

--- build.sh
+++ build.sh
@@ -16,6 +16,8 @@ pushd /tmp/build
 # Download the clamav package that includes unrar
 curl -L --output clamav.rpm "https://www6.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/clamav-${VERSION}.el7.art.x86_64.rpm"
 rpm2cpio clamav.rpm | cpio -vimd
+curl -L --output clamd.rpm "https://www6.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/clamd-${VERSION}.el7.art.x86_64.rpm"
+rpm2cpio clamd.rpm | cpio -vimd
 
 # Download required glibc libs
 curl -L --output glibc-2.17-317.el7.x86_64.rpm https://www.rpmfind.net/linux/centos/7.9.2009/os/x86_64/Packages/glibc-2.17-317.el7.x86_64.rpm
@@ -59,10 +61,14 @@ popd
 
 mkdir -p bin lib data
 
-cp /tmp/build/usr/bin/clamscan /tmp/build/usr/bin/freshclam bin/.
+cp /tmp/build/usr/bin/clamscan bin/.
+cp /tmp/build/usr/bin/freshclam bin/.
+cp /tmp/build/usr/bin/clamdscan bin/.
+cp /tmp/build/usr/sbin/clamd bin/.
 cp -R /tmp/build/usr/lib64/* lib/.
 cp -R /tmp/build/lib64/* lib/.
 cp freshclam.conf bin/freshclam.conf
+cp /tmp/build/etc/clamd.conf bin/clamd.conf
 
 # Download current virus signatures
 LD_LIBRARY_PATH=/lib64:/usr/lib64:$LD_LIBRARY_PATH:$PWD/lib bin/freshclam --config-file=$PWD/bin/freshclam.conf --datadir=$PWD/data
